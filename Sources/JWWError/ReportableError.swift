1da62cabadfcfdbf4dc54ea4b347177531cc951d
Justin Williams <justinw@me.com>
2020-08-18 11:03:58 -0600

    Conform DecodingError to ReportableError

--- Sources/JWWError/ReportableError.swift
+++ Sources/JWWError/ReportableError.swift
@@ -26,3 +26,50 @@ extension ReportableError {
         return true
     }
 }
+
+// MARK: DecodingError Helpers
+// ====================================
+// DecodingError Helpers
+// ====================================
+
+extension DecodingError: ReportableError {
+    public static var domain: String {
+        "com.justinwme.error.json-decoding"
+    }
+
+    public var code: Int {
+        let result: Int
+        switch self {
+        case .dataCorrupted:
+            result = 0
+        case .keyNotFound:
+            result = 1
+        case .typeMismatch:
+            result = 2
+        case .valueNotFound:
+            result = 3
+        @unknown default:
+            result = -1
+        }
+
+        return result
+    }
+
+    public var message: String {
+        let result: String
+        switch self {
+        case .dataCorrupted(let context):
+            result = "Data corrupted: \(context.debugDescription)"
+        case .keyNotFound(let key, let context):
+            result = "Key '\(key.stringValue)' not found. \(context.debugDescription)"
+        case .typeMismatch(let type, let context):
+            result = "Type mismatch. \(type) expected. \(context.debugDescription)"
+        case .valueNotFound(let type, let context):
+            result = "Value of type \(type) not found. \(context.debugDescription)"
+        @unknown default:
+            result = "JSONDecoding Error of unknown type \(self)"
+        }
+
+        return result
+    }
+}
