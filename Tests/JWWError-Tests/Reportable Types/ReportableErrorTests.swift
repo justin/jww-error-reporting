d74d9994d8c47083166bade33e9d7175ca0e7260
Justin Williams <justinw@me.com>
2021-01-20 13:31:21 -0700

    Add a few more cases to ReportableErrorTests

--- Tests/JWWError-Tests/Reportable Types/ReportableErrorTests.swift
+++ Tests/JWWError-Tests/Reportable Types/ReportableErrorTests.swift
@@ -3,22 +3,72 @@ import XCTest
 
 // Tests to validate our `ReportableError` protocol.
 final class ReportableErrorTests: XCTestCase {
-    private struct TestReportableError: ReportableError {
-        static let domain: String = UUID().uuidString
-        let code: Int
-        let message: String
-        let userInfo: [ErrorPayloadKey: AnyHashable]
+    /// Validate that a type that conforms to ReportableError defaults the value of `isReportable` to true.
+    func testErrorsDefaultToReportable() throws {
+        let error = TestErrorStruct()
+        XCTAssertTrue(error.isReportable)
+    }
 
-        init(code: Int = Int.random(in: 0..<100), message: String = UUID().uuidString, userInfo: [ErrorPayloadKey: AnyHashable] = [:]) {
-            self.code = code
-            self.message = message
-            self.userInfo = userInfo
+    /// Validate we can parse the enum based TestError.
+    func testRetrievingTestErrorValues() throws {
+        let error = TestErrorEnum.reason1
+
+        XCTAssertEqual(error.code, 0)
+        XCTAssertEqual(error.message, "Reason 1")
+        XCTAssertTrue(error.userInfo.isEmpty)
+    }
+
+    /// Validate we can retrieve the associated value as part of an error
+    func testParsingAssociatedValue() throws {
+        let error = TestErrorEnum.reason2(value: "Associated Value for Reason 3")
+        XCTAssertEqual(error.code, 1)
+        XCTAssertEqual(error.message, "Associated Value for Reason 3")
+        XCTAssertTrue(error.userInfo.isEmpty)
+    }
+}
+
+/// Custom error type that is build from a value type we can inject values into.
+private struct TestErrorStruct: ReportableError {
+    static let domain: String = "com.justinwme.tests.reportable-error"
+    let code: Int
+    let message: String
+    let userInfo: [ErrorPayloadKey: AnyHashable]
+
+    init(code: Int = Int.random(in: 0..<100), message: String = UUID().uuidString, userInfo: [ErrorPayloadKey: AnyHashable] = [:]) {
+        self.code = code
+        self.message = message
+        self.userInfo = userInfo
+    }
+}
+
+/// Enum based error type that has three predefined values.
+private enum TestErrorEnum: Swift.Error {
+    case reason1
+    case reason2(value: String)
+}
+
+extension TestErrorEnum: ReportableError {
+    static let domain: String = "com.justinwme.tests.test-error"
+
+    var code: Int {
+        switch self {
+        case .reason1:
+            return 0
+        case .reason2:
+            return 1
         }
     }
 
-    /// Validate that a type that conforms to ReportableError defaults the value of `isReportable` to true.
-    func testErrorsDefaultToReportble() throws {
-        let error = TestReportableError()
-        XCTAssertTrue(error.isReportable)
+    var message: String {
+        switch self {
+        case .reason1:
+            return "Reason 1"
+        case .reason2(let value):
+            return value
+        }
+    }
+
+    var userInfo: [ErrorPayloadKey : AnyHashable] {
+        [:]
     }
 }
