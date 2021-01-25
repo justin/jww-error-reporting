ca71251cfc8ac1f14ec59bfaa86dcf7de3fdb6a5
Justin Williams <justinw@me.com>
2021-01-25 13:43:48 -0700

    Add missing documentation to ErrorPayloadKey

--- Sources/JWWError/ErrorPayloadKey.swift
+++ Sources/JWWError/ErrorPayloadKey.swift
@@ -1,20 +1,28 @@
 import Foundation
 
+/// Value type that declares the different payload keys that can be used in a `ReportableError.userInfo` payload.
 public struct ErrorPayloadKey: Hashable, Equatable, RawRepresentable {
-    public typealias RawValue = String
-    public var rawValue: RawValue
+    public let rawValue: String
 
-    public init(rawValue: RawValue) {
+    /// Creates a new instance with the specified raw value.
+    ///
+    /// - Parameter rawValue: The raw value to use for the new instance.
+    public init(_ rawValue: String) {
         self.rawValue = rawValue
     }
 
-    public init(_ rawValue: RawValue) {
+    public init(rawValue: String) {
         self.rawValue = rawValue
     }
 }
 
 public extension ErrorPayloadKey {
+    /// A key that declares the error domain, as an `String`.
     static let domainKey = ErrorPayloadKey(rawValue: "domain")
+
+    /// A key that declares the error code, as an `Int`.
     static let codeKey = ErrorPayloadKey(rawValue: "code")
+
+    /// A key that declares the error message, as a `String`.
     static let messageKey = ErrorPayloadKey(rawValue: "message")
 }
