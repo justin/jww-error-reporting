bc3f15f4daafca484bf8e222d7a3d0ae060f0ab3
Justin Williams <justinw@me.com>
2021-01-28 13:31:20 -0700

    Add support for passing an additional info payload with an error

--- Sources/JWWError/ErrorParser.swift
+++ Sources/JWWError/ErrorParser.swift
@@ -12,15 +12,22 @@ extension ErrorReporter {
         /// Information about the application that is reporting the error.
         let appInfo: AppInfoProviding
 
+        /// Supplemental key/value pairs that can be associated with the error payload.
+        let additionalInfo: [ErrorPayloadKey: AnyHashable]
+
         /// Creates the logstash message that will be send to the reporting service.
         func makeLogstashMessage() throws -> ErrorPayload {
-            ErrorPayload(domain: type(of: error).domain,
+            let mergedPayload = error.userInfo.merging(additionalInfo) { (left, right) -> AnyHashable in
+                return right
+            }
+
+            return ErrorPayload(domain: type(of: error).domain,
                          code: error.code,
                          message: error.message,
                          environment: appInfo.environment,
                          date: Date(),
                          app: ErrorPayload.AppInfo(appInfo),
-                         metadata: ErrorPayloadMetadata(values: error.userInfo))
+                         metadata: ErrorPayloadMetadata(values: mergedPayload))
         }
     }
 }
--- Tests/JWWError-Tests/ErrorParserTests.swift
+++ Tests/JWWError-Tests/ErrorParserTests.swift
@@ -3,6 +3,7 @@ import XCTest
 
 /// Tests to validate our `ErrorReporter` class.
 final class ErrorParserTests: XCTestCase {
+
     private struct TestError: ReportableError {        
         static let domain: String = "com.justinwme.jwwerror.test-error"
         let code: Int
@@ -17,7 +18,7 @@ final class ErrorParserTests: XCTestCase {
                               userInfo: [:])
         let info = AppInfoFixture.fixture()
 
-        let parser = ErrorReporter.Parser(error: error, appInfo: info)
+        let parser = ErrorReporter.Parser(error: error, appInfo: info, additionalInfo: [:])
 
         let result = try XCTUnwrap(parser.makeLogstashMessage())
 
@@ -27,5 +28,65 @@ final class ErrorParserTests: XCTestCase {
         XCTAssertEqual(result.domain, TestError.domain)
         XCTAssertEqual(result.message, error.message)
         XCTAssertEqual(result.app.platform, info.platform)
+    }
+
+    /// Validate the additional info payload is included in generated message.
+    func testAdditionalInfoPayloadIsIncluded() throws {
+        let fooKey = ErrorPayloadKey("foo-key")
+        let barKey: ErrorPayloadKey = ErrorPayloadKey("bar-key")
+        let expectedPayload: [ErrorPayloadKey: AnyHashable] = [
+            fooKey: "Foo",
+            barKey: "Bar"
+        ]
+
+        let error = TestError(code: Int.random(in: 0..<100),
+                              message: UUID().uuidString,
+                              userInfo: [fooKey: "Foo"])
+        let additionalInfo: [ErrorPayloadKey: AnyHashable] = [
+            barKey: "Bar"
+        ]
+
+        let parser = ErrorReporter.Parser(error: error,
+                                          appInfo: AppInfoFixture.fixture(),
+                                          additionalInfo: additionalInfo)
+
+        let result = try XCTUnwrap(parser.makeLogstashMessage()).metadata
+
+        XCTAssertEqual(result.values.count, expectedPayload.count)
+        for (key, value) in result.values {
+            XCTAssertEqual(expectedPayload[key], value)
+        }
+    }
+
+    /// Validate that we prioritize the additionalInfo payload value over the error value for metadata.
+    func testOverridingErrorUserInfoWithAdditonalInfo() throws {
+        let fooKey = ErrorPayloadKey("foo-key")
+        let barKey: ErrorPayloadKey = ErrorPayloadKey("bar-key")
+        let expectedPayload: [ErrorPayloadKey: AnyHashable] = [
+            fooKey: "Foo",
+            barKey: "Biz"
+        ]
+
+
+        let error = TestError(code: Int.random(in: 0..<100),
+                              message: UUID().uuidString,
+                              userInfo: [
+                                fooKey: "Foo",
+                                barKey: "Bar"
+                              ])
+        let additionalInfo: [ErrorPayloadKey: AnyHashable] = [
+            barKey: "Biz"
+        ]
+
+        let parser = ErrorReporter.Parser(error: error,
+                                          appInfo: AppInfoFixture.fixture(),
+                                          additionalInfo: additionalInfo)
+
+        let result = try XCTUnwrap(parser.makeLogstashMessage()).metadata
+
+        XCTAssertEqual(result.values.count, expectedPayload.count)
+        for (key, value) in result.values {
+            XCTAssertEqual(expectedPayload[key], value)
+        }
     }
 }
